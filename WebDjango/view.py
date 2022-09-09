9d93ee520f73b43dce1f14f46a7063d5f67fecd4
Francisco Vega <franvega25005@gmail.com>
2022-09-08 21:53:41 -0300

    Inicializacion proyecto WebDjango

---
+++ WebDjango/view.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def home(request, name):
+    return HttpResponse(f"Hola soy {name}")
+
+def homePage(self):
+    lista = [1,2,3,4,5,6,7,8,9]
+    data = {'nombre':'Francisco', 'apellido': 'Vega', 'lista': lista}
+    planilla = loader.get_template('home.html')
+    documento = planilla.render(data)
+    return HttpResponse(documento)
